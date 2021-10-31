db2b1201d76746547808ac3b45ca928694ab2595
AnamikaTiwari29 <89921049+AnamikaTiwari29@users.noreply.github.com>
2021-10-31 21:01:24 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,24 @@
+class Box
+  {
+    constructor()
+    {
+      this.x =100;
+      this.y = 200
+      this.w = 50;
+      this.h = 50;
+    }
+    
+    show()
+    {
+      rect(this.x,this.y,this.w,this.h)
+    }
+    
+    //write function to set the x position of the box
+   size_add(r){
+   this.x=this.x+3;
+   }
+
+
+  }
+
+  
